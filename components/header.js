928181db16001c574a6fcf6cfb166be6d4fd38d7
Arshcode99 <Arshcode99@gmail.com>
2022-12-12 19:42:11 +0530

    nav

--- components/header.js
+++ components/header.js
@@ -18,7 +18,7 @@ const Header = () => {
           <div className="hidden w-full md:block md:w-auto" id="navbar-default">
             <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 bg-mock-600">
               <li>
-                <a href="#" className="block py-2 pl-3 pr-4 text-blue-600 bg-blue-100 rounded md:bg-transparent md:text-blue-600 md:p-0 dark:text-white" aria-current="page">Home</a>
+                <a href="#" className="block py-2 pl-3 pr-4 text-blue-600 bg-blue-100 rounded md:bg-transparent md:text-blue-600 md:p-0 ">Home</a>
               </li>
               <li>
                 <a href="#services" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
