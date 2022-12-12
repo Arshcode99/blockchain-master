e3c26afbb71a8907b912395d703e4ce0e5e89e93
Arshcode99 <Arshcode99@gmail.com>
2022-12-12 21:24:43 +0530

    docs

--- pages/index.js
+++ pages/index.js
@@ -298,7 +298,9 @@ export default function Home() {
         </svg>
       </div>
     </div>
-    <a href='#contact' class="flex mx-auto mt-20 text-white bg-blue-600 border-0 py-2 px-8 focus:outline-none hover:blue-600 rounded text-lg">Let's Talk</a>
+    <a  href='#contact' >
+    <button class="flex mx-auto mt-20 text-white bg-blue-600 border-0 py-2 px-8 focus:outline-none hover:blue-600 rounded text-lg">Let's Talk</button>
+    </a>
   </div>
      </section>
      <section class="text-gray-600 body-font" id='benifits'>
